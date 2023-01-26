142ee7b64443ee2baf3f27a7f44419039af3de20
uku393 <uku.kovamaa@tktk.ee>
2023-01-26 16:50:53 +0200

    create new app

---
+++ MyFirstApp2/Program.cs
@@ -0,0 +1,13 @@
+﻿//This is a comment to my code
+
+//rakendus küsib kasutajal sisestada tema nime
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Sisesta oma nimi:");               //Output
+//string - sõne
+string userName = Console.ReadLine();                 //Input
+
+Console.WriteLine("Tere" + ", " + userName + "!");    //Output
+
+//string interpolation
+Console.WriteLine($"Tere, {userName}!");              //Output
